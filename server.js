15edb53fd14fa19236d6c11ac2d0465b7fb101e3
LinYunHsuan <42128428+LinYunHsuan@users.noreply.github.com>
2018-12-18 14:31:29 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ var options = {
     encoding: 'utf8'
 };
 
+//連接到MLab
 MongoClient.connect(mongoUrl, { useNewUrlParser: true }, function (err, client) {
 			if(err) throw err;
 			else{
@@ -43,10 +44,10 @@ client.on('connect', function() { // When connected
     // subscribe to a topic
     client.subscribe('RaspBerry');
     client.subscribe('RaspBerry/paid');
-    // when a message arrives, do something with it
-
 });
+// when a message arrives, do something with it
 client.on('message', function(topic, msg) {
+	//新增停車格編號
 	if(topic === 'RaspBerry'){
 		var splitMessage = split(msg.toString(),{ separator: ',' });
             var inLicense = splitMessage[0];
@@ -62,26 +63,25 @@ client.on('message', function(topic, msg) {
             console.log('insert success:');
             });
 	}
+	//查詢是否付費
 	if(topic === 'RaspBerry/paid'){
 		console.log(msg.toString());
 		myDB.collection('QRCode',function(err,collection){
 			collection.find({Pay:"Y"}).toArray(function(err,items){
             if(err) {
             	client.publish('RaspBerry/back',"0",{qos:1},function(err){
-                if(err) throw err;
-            })
-
+                if(err) throw err;}
+                )
             }
             else{
             	client.publish('RaspBerry/back',"Y",{qos:1},function(err){
-            		if(err) throw err;
-            	});
+            		if(err) throw err;}
+            	);
             }
         });
-		});
+        });
 	};    
 });
-var qrDB;
 
 //reservation insert
 app.get('/api/insert', function(req, res){
@@ -94,58 +94,6 @@ app.get('/api/insert', function(req, res){
 		if(err) throw err;
 		collection.insertOne({LicensePlate:strlicense,ReservationTime:strtime});
 		res.status(200).end();
-	});
-});
-
-//order insert
-app.get('/api/order_insert', function(req, res){
-	var strlicense = req.query.license;
-	console.log(strlicense);
-	var parking_space = 
-	console.log(parking_space);
-
-	myDB.collection("Order", function(err, collection){
-		if(err) throw err;
-		collection.insertOne({LicensePlate:strlicense, parking_space:parking_space});
-		res.status(200).end();
-	});
-});
-//Order finish query
-app.get('/api/queryOrder', function(req,res){
-	var queryLicense = req.query.license;
-	myDB.collection("LicensePlate",function(err,collection){
-        collection.find({LicensePlate:queryLicense}).toArray(function(err,items){
-            if(err) throw err;
-            console.log(items);
-            res.send(items).end();
-        });
-    });
-})
-//change reservation
-app.get('/api/update', function(req, res){
-	var strlicense = req.query.license;
-	var strtime = req.query.time;
-	myDB.collection("Order", function(err, collection){
-		collection.find({LicensePlate:strlicense}).toArray(function(err,items){
-			if(err) throw err;
-			collection.update({LicensePlate:strlicense},{$set:{TimeReservation:strtime}},{w:1}, function(err,result){
-				if(err) throw err;
-				res.send(strtime).end();
-			});
-			
-		});
-	});
-});
-
-//delete reservation
-app.get('/api/delete', function(req,res){
-	var strlicense = req.query.license;
-	console.log(strlicense);
-	myDB.collection("Order", function(err,collection){
-		collection.remove({LicensePlate:strlicense},{w:1},function(err,items){
-			if(err) throw err;
-			res.send(strlicense).end();
-		});
 	});
 });
 
@@ -191,7 +139,7 @@ app.get('/api/paycheck', function(req, res){
 });
 
 
-//ppsition query
+//position query
 app.get('/api/PosQuery', function(req, res){
 	var checkLi = req.query.check;
 	myDB.collection("find",function(err,collection){
